32894f9b8a8adb9fbd78344ecaef88ac8b64b435
yuralala8 <yc1435@nyu.edu>
2017-08-07 16:36:28 -0400

    javascript arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+// function kittens() {
+//   var kittens = ["Milo", "Otis", "Garfield"]
+//   return kittens
+// }
+
+function destructivelyAppendKitten(name) {
+  kittens = [...kittens, name]
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var kitties
+  kitties = [...kittens, name]
+  return kitties
+}
+
+function prependKitten(name) {
+  var kitties
+  kitties = [name, ...kittens]
+  return kitties
+}
+
+function removeLastKitten() {
+  var kitties
+  kitties = kittens.slice(0, kittens.length - 1)
+  return kitties
+}
+
+function removeFirstKitten() {
+  var kitties
+  kitties = kittens.slice(1)
+  return kitties
+}
